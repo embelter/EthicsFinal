eb958292c428fc4f58bbff4d7668f78c13d4bd71
Eric <eric.belter@live.com>
2018-05-01 18:25:44 -0500

    - Scaffolding for API call now fully in place - API call displayed - Updated CSS

--- js/base.js
+++ js/base.js
@@ -1,22 +1,10 @@
 $(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    let city = $('#location').val();
-    $('#location').val("");
     $.ajax({
-      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=API-KEY-GOES-HERE`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-      },
-      error: function() {
-        $('#errors').text("There was an error processing your request. Please try again.")
-      }
+        url: "http://rest-service.guides.spring.io/greeting"
+    }).then(function(data) {
+       $('.greeting-id').append(data.id);
+       $('.greeting-content').append(data.content);
     });
-  });
 });
 function update(){
 	$("span").css("background-color", "lightgrey");
