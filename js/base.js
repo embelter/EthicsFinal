25f0237ef87a2b27c389c473fd055eb99b8828d9
Eric <eric.belter@live.com>
2018-05-02 22:38:30 -0500

    - Added generic div creator in js file so html does not get cluttered - Added gitignore file so node modules do not get pushed

--- js/base.js
+++ js/base.js
@@ -5,13 +5,21 @@ $(document).ready(function() {
        $('.greeting-id').append(data.id);
        $('.greeting-content').append(data.content);
     });
+    var titleDiv1 = createDivElement(200, 50, "black", 5);
+    var titleDiv2 = createDivElement(300, 50, "black", 15);
+	$("body").append(titleDiv1);
+	$("body").append(titleDiv2);
 });
-function update(){
-	$("span").css("background-color", "lightgrey");
-	$("div").each(function(i){
-		var $input = $(this).children("input:first");
-		var $value = $input.val();
-		var filter = "span:lt(" + $value + ")";
-		$input.siblings(filter).css("background-color", "blue");
-	})
-}
+
+function createDivElement (w, h, c, p) { 
+
+	  var newDiv = document.createElement("div"); 
+	  $(newDiv).css("position", "absolute");
+	  $(newDiv).css("background-color", c);
+	  $(newDiv).css("width", w + "px");
+	  
+	  $(newDiv).css("height", h + "px");
+	  $(newDiv).css("top", p + "%");
+	  $(newDiv).css("border-radius", 15 + "px");
+	  return newDiv;
+	}
