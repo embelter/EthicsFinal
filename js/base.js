181ad4a4227150786eff7fff7fcf53a34e5d86a4
Eric <eric.belter@live.com>
2018-05-05 23:05:40 -0500

    - Updated gitignore file to include eclipse project files - Removed code clutter from index.html - Removed greeting class alterations from css page - Added a body div via base.js and altered API call to IP lookup - Added a color and fontsize property to addTextToMe function

--- js/base.js
+++ js/base.js
@@ -1,17 +1,21 @@
+// The following block of code is loaded once webpage is completed
 $(document).ready(function() {
     $.ajax({
-        url: "http://rest-service.guides.spring.io/greeting"
+        url: "https://api.ipify.org?format=json"
     }).then(function(data) {
-       $('.greeting-id').append(data.id);
-       $('.greeting-content').append(data.content);
+       $("#bodyCard1").append(data.ip);
     });
-    var titleDiv1 = createDivElement(200, 50, "black", 5);
-    var titleDiv2 = createDivElement(300, 50, "black", 15);
-	$("body").append(addTextToMe(titleDiv1, "Something Something"));
-	$("body").append(addTextToMe(titleDiv2, "Edward Snowden"));
+    var titleDiv1 = createDivElement(500, 50, "black", 5, "titleCard1");
+    var titleDiv2 = createDivElement(400, 50, "black", 15, "titleCard2");
+    var titleDiv3 = createDivElement(300, 500, "black", 25, "bodyCard1");
+    
+	$("body").append(addTextToMe(titleDiv1, "Something Something", "Aqua", "30px"));
+	$("body").append(addTextToMe(titleDiv2, "Edward Snowden", "Aqua", "30px"));
+	$("body").append(addTextToMe(titleDiv3));	
 });
 
-function createDivElement (w, h, c, p) { 
+// Creates a basic div element with indicated properties
+function createDivElement (w, h, c, p, id) { 
 
 	  var newDiv = document.createElement("div"); 
 	  $(newDiv).css("position", "absolute");
@@ -21,12 +25,15 @@ function createDivElement (w, h, c, p) {
 	  $(newDiv).css("height", h + "px");
 	  $(newDiv).css("top", p + "%");
 	  $(newDiv).css("border-radius", 15 + "px");
+	  $(newDiv).attr("id", id);
 	  return newDiv;
 	}
-function addTextToMe (me, string)	{
+// Adds text to an object named me
+function addTextToMe (me, string, color, fontSize)	{
 	var addMe = document.createElement("font");
 	$(addMe).append(string);
-	$(addMe).css("color", "white");
+	$(addMe).css("color", color);
+	$(addMe).css("font-size", fontSize);
 	$(me).append(addMe);
 	return me;
 }
