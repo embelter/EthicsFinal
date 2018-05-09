31e93658b3fa560778b33965a5092ef5219c663d
Eric <eric.belter@live.com>
2018-05-09 18:07:07 -0500

    - Added second display div - Displays users weather information

--- js/base.js
+++ js/base.js
@@ -6,26 +6,43 @@ $(document).ready(function() {
        $("#bodyCard1").append(data.query + "</br>");
        $("#bodyCard1").append(data.country + "</br>");
        $("#bodyCard1").append(data.zip + "</br>");
+       $("#bodyCard1").append(data.region + "</br>");
        $("#bodyCard1").append(data.city + "</br>");
+       
+       
+       fillSecondDiv(data.region, data.city);
     });
-    var titleDiv1 = createDivElement(500, 50, "black", 5, "titleCard1");
-    var titleDiv2 = createDivElement(400, 50, "black", 15, "titleCard2");
-    var titleDiv3 = createDivElement(300, 500, "black", 25, "bodyCard1");    
+    
+    var titleDiv1 = createDivElement(500, 50, "black", 5, "titleCard1", 2);
+    var titleDiv2 = createDivElement(400, 50, "black", 15, "titleCard2", 2);
+    var titleDiv3 = createDivElement(300, 500, "black", 25, "bodyCard1", 2);  
+    var titleDiv4 = createDivElement(300, 500, "black", 25, "bodyCard2", 52); 
+    
 	$("body").append(addTextToMe(titleDiv1, "Something Something", "Aqua", "30px"));
 	$("body").append(addTextToMe(titleDiv2, "Edward Snowden", "Aqua", "30px"));
-	$("body").append(addTextToMe(titleDiv3));		
+	$("body").append(addTextToMe(titleDiv3));
+	$("body").append(addTextToMe(titleDiv4));
 });
-
+function fillSecondDiv(region, city)	{
+    $.ajax({
+        url: "http://api.wunderground.com/api/5c9f8cd57ad7d518/conditions/q/" + region + "/" + city + ".json"
+    }).then(function(data) {
+       $("#bodyCard2").append("The weather in " + city + ":</br>");
+       $("#bodyCard2").append("Temp: " + data.current_observation.temp_f + "</br>");
+       $("#bodyCard2").append("And: " + data.current_observation.icon + "</br>");
+    });
+}
 // Creates a basic div element with indicated properties
-function createDivElement (w, h, c, p, id) { 
+function createDivElement (w, h, c, posTop, id, posLeft) { 
 
 	  var newDiv = document.createElement("div"); 
 	  $(newDiv).css("position", "absolute");
 	  $(newDiv).css("background-color", c);
-	  $(newDiv).css("width", w + "px");
-	  
+	  $(newDiv).css("width", w + "px");	  
 	  $(newDiv).css("height", h + "px");
-	  $(newDiv).css("top", p + "%");
+	  
+	  $(newDiv).css("top", posTop + "%");
+	  $(newDiv).css("left", posLeft + "%");	  
 	  $(newDiv).css("border-radius", 15 + "px");
 	  $(newDiv).attr("id", id);
 	  return newDiv;
